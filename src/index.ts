d80d5ad38c2709b14b5b76c6e3e108ad8256fa08
Oscar Munoz Bernales <omunoz4@bancoestado.cl>
2024-03-13 22:42:53 -0300

    UPD: Se agrega un ejemplo con el operador map

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,19 @@
-import { Observer, of } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { Observer, fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-const observe: Observer<number> = {
+const observe: Observer<any> = {
     next: value => console.log(`Next: ${value}`),
     error: error => console.error(`Error: ${error}`),
     complete: () => console.log('Complete')
 }
 
-
-// Crear un Observable que emite números
-const source$ = of(1, 2, 3, 4, 5);
+// Crear un Observable que emite eventos de teclado
+const keyUp$ = fromEvent<KeyboardEvent>(document, 'keyup');
 
 // Utilizar el operador map para transformar los valores emitidos por el Observable
-const example$ = source$.pipe(
-    map(value => value * 10)
+const codeKey$ = keyUp$.pipe(
+    map(event => event.code)
 );
 
 // Suscribirse al Observable y registrar los valores emitidos
-example$.subscribe(observe);
-
-// Salida: 10, 20, 30, 40, 50
+codeKey$.subscribe(observe);
--- src/seccion3/1.-map.ts
+++ src/seccion3/1.-map.ts
@@ -1,7 +1,7 @@
-import { Observer, of } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { Observer, fromEvent, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-const observe: Observer<number> = {
+const observe: Observer<any> = {
     next: value => console.log(`Next: ${value}`),
     error: error => console.error(`Error: ${error}`),
     complete: () => console.log('Complete')
@@ -13,10 +13,21 @@ const source$ = of(1, 2, 3, 4, 5);
 
 // Utilizar el operador map para transformar los valores emitidos por el Observable
 const example$ = source$.pipe(
-    map(value => value * 10)
+    map<number, string>(value => `${value}`)
 );
 
 // Suscribirse al Observable y registrar los valores emitidos
 example$.subscribe(observe);
 
 // Salida: 10, 20, 30, 40, 50
+
+// Crear un Observable que emite eventos de teclado
+const keyUp$ = fromEvent<KeyboardEvent>(document, 'keyup');
+
+// Utilizar el operador map para transformar los valores emitidos por el Observable
+const codeKey$ = keyUp$.pipe(
+    map(event => event.code)
+);
+
+// Suscribirse al Observable y registrar los valores emitidos
+codeKey$.subscribe(observe);
